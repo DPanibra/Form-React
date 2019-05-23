a6667dbc92ba23353e8c64427d3195f6ea5162aa
DPanibra <dpanibra@unsa.edu.pe>
2019-05-23 15:12:43 -0500

    Formulario en React v.1

---
+++ precache-manifest.1f6ed0534f572c58c6f430b777bea311.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73a76a6bd0a63438c61644e04207d83f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6cf34fe2d7545784e723",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "92625e8f421b478da1a1",
+    "url": "/static/js/2.5ca6e9a6.chunk.js"
+  },
+  {
+    "revision": "6cf34fe2d7545784e723",
+    "url": "/static/js/main.6f1ae8b6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
